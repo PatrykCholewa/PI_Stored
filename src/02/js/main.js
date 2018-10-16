6f33cbe6b2ef64836c2b97535ccea49417ba5a37
Patryk Cholewa <patryk.cholewa@sharpeo.com>
2018-10-16 23:41:37 +0200

    commit 02

--- src/02/js/main.js
+++ src/02/js/main.js
@@ -1,4 +1,4 @@
-// http://edi.iem.pw.edu.pl/chaberb/register/check/
+const regLink = "http://edi.iem.pw.edu.pl/chaberb/register/check/";
 
 init();
 
@@ -7,29 +7,18 @@ function init(){
     const pesel = document.getElementById("pesel");
     const password = document.getElementById("password");
     const repeat_password = document.getElementById("repeat-password");
-    login.addEventListener("change", check_login_availability, false);
+    login.addEventListener("change", checkLoginAvailability, false);
     pesel.addEventListener("change", setSexByPesel, false);
     password.addEventListener("change", checkPasswordMatch, false);
     repeat_password.addEventListener("change", checkPasswordMatch, false);
 }
 
-function check_login_availability(){
-    //@TODO
-    const loginLi = document.getElementById("login-li");
-    let div = document.getElementById("login-status");
-
-    if( div == null ){
-        div = document.createElement( 'div' );
-        div.innerText = "OK";
-        div.id = "login-status";
-        const clsList = div.classList;
-        clsList.add("valid-input");
-        clsList.add("input-status");
-        loginLi.appendChild(div);
-    }
-
-
-
+function checkLoginAvailability(){
+    const loginInput = document.getElementById("login").value;
+    const regUrl = regLink + loginInput;
+    const ret = fetch(regUrl)
+    .then(response => {response.json()
+    .then(data => loginFreeResponseHandle(data,loginInput) )});
 }
 
 function setSexByPesel(){
@@ -42,4 +31,36 @@ function checkPasswordMatch(){
 
 function checkPasswordStrength(){
     //@TODO
+}
+
+function loginFreeResponseHandle(data,login){
+    let json = data;
+    let div = document.getElementById("login-status");
+
+    if( div == null ){
+        createNewLoginStatus();
+        div = document.getElementById("login-status");
+    }
+
+    const cls = div.classList;
+
+    if( json[login] ){
+        cls.remove("valid-input");
+        cls.add("invalid-input");
+        div.innerText = "NOK";
+    } else {
+        cls.remove("invalid-input");
+        cls.add("valid-input");
+        div.innerText = "OK";
+    }
+
+}
+
+function createNewLoginStatus(){
+    const loginLi = document.getElementById("login-li");
+    let div = document.createElement('div');
+
+    div.id = "login-status";
+    div.classList.add("input-status");
+    loginLi.appendChild(div);
 }
