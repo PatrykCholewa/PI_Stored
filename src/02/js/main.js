047bb35809925a38f688d1ed985b0948db65f093
Patryk Cholewa <patryk.cholewa@sharpeo.com>
2018-10-17 22:41:09 +0200

    finish 02

--- src/02/js/main.js
+++ src/02/js/main.js
@@ -10,7 +10,7 @@ function init(){
     const myForm = document.getElementById("my-form");
     login.addEventListener("change", checkLoginAvailability, false);
     pesel.addEventListener("change", setSexByPesel, false);
-    password.addEventListener("change", checkPasswordMatch, false);
+    password.addEventListener("input", checkPasswordMatch, false);
     password.addEventListener("change", checkPasswordStrength, false);
     repeat_password.addEventListener("change", checkPasswordMatch, false);
     myForm.addEventListener("submit", preventFormSubmitIfNotValid, false);
@@ -18,8 +18,34 @@ function init(){
 
 function preventFormSubmitIfNotValid(event){
     if( !checkPasswordMatch() ){
-        return event.preventDefault();
+        event.preventDefault();
+        alert("Aborted submit!\nInvalid data");
     }
+
+    const flag = checkLoginAvailabilityWithControl();
+
+    if( flag !== true ) {
+        event.preventDefault();
+        alert("Login taken!");
+    } else {
+        alert("Success!");
+    }
+}
+
+function checkLoginAvailabilityWithControl(){
+    const login = document.getElementById("login").value;
+    const regUrl = regLink + login;
+    let ret = undefined;
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const json = JSON.parse(this.responseText);
+        ret = !json[login];
+    }
+  };
+    req.open("GET", regUrl , false );
+    req.send();
+    return ret;
 }
 
 function checkLoginAvailability(){
