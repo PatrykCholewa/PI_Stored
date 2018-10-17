990fcc93f156fab11bbadaf5f9e9ff1a537220a4
Patryk Cholewa <patryk.cholewa@sharpeo.com>
2018-10-17 13:19:12 +0200

    commit 02

--- src/02/js/main.js
+++ src/02/js/main.js
@@ -9,7 +9,9 @@ function init(){
     const repeat_password = document.getElementById("repeat-password");
     login.addEventListener("change", checkLoginAvailability, false);
     pesel.addEventListener("change", setSexByPesel, false);
+    password.addEventListener("key-up", checkPasswordStrength, false);
     password.addEventListener("change", checkPasswordMatch, false);
+    password.addEventListener("change", checkPasswordStrength, false);
     repeat_password.addEventListener("change", checkPasswordMatch, false);
 }
 
@@ -30,13 +32,31 @@ function checkPasswordMatch(){
 }
 
 function checkPasswordStrength(){
-    //@TODO
+    let meter = document.getElementById("password-meter");
+    const password = document.getElementById("password").value;
+
+    if( meter == null ){
+        meter = createNewPasswordMeter();
+    }
+
+    meter.value = 0;
+    if( /\d/.test(password) ){
+        meter.value = meter.value + 0.25;
+    }
+    if( password.length > 7 ){
+        meter.value = meter.value + 0.25;
+    }
+    if( /[a-z]/.test(password) ){
+        meter.value = meter.value + 0.25;
+    }
+    if( /[A-Z]/.test(password) ){
+        meter.value = meter.value + 0.25;
+    }
 }
 
 function loginFreeResponseHandle(data,login){
     let json = data;
     let div = document.getElementById("login-status");
-
     if( div == null ){
         createNewLoginStatus();
         div = document.getElementById("login-status");
@@ -63,4 +83,16 @@ function createNewLoginStatus(){
     div.id = "login-status";
     div.classList.add("input-status");
     loginLi.appendChild(div);
+}
+
+function createNewPasswordMeter(){
+    const passwordLi = document.getElementById("password-li");
+    let meter = document.createElement("meter");
+
+    meter.id = "password-meter";
+    meter.low = 0.3;
+    meter.high = 0.8;
+    meter.optimum = 0.9;
+    passwordLi.appendChild(meter);
+    return meter;
 }
