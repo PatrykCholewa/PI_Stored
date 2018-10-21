1a058b43882e858d29400d8047754879622f0410
Patryk Cholewa <patryk.cholewa@sharpeo.com>
2018-10-21 17:09:10 +0200

    login temp

--- resource/js/login.js
+++ resource/js/login.js
@@ -3,8 +3,20 @@ $(function() {
     $("#sign-up").click(redirectToRegister);
 });
 
-function login() {
-
+function login(event) {
+    event.preventDefault();
+    console.log("OK");
+    fetch("ws/login/", {
+        method: "POST",
+        form: event.form
+    }).then( response => {
+        if (response.statusCode === 200) {
+            window.location = "list";
+        } else {
+            console.log("NOK");
+        }
+    });
+    return false;
 }
 
 function redirectToRegister() {
