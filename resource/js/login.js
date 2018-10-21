6581f091c4624119763b2ac29594c12c3545109e
Patryk Cholewa <patryk.cholewa@sharpeo.com>
2018-10-21 19:04:38 +0200

    fix id

--- resource/js/login.js
+++ resource/js/login.js
@@ -1,19 +1,18 @@
-$(function() {
+$(function(){
     $("#login-form").submit(login);
     $("#sign-up").click(redirectToRegister);
 });
 
 function login(event) {
     event.preventDefault();
-    console.log("OK");
     fetch("ws/login/", {
         method: "POST",
-        form: event.form
+        body: "user-id:cholewp1\npassword=pass"
     }).then( response => {
         if (response.statusCode === 200) {
             window.location = "list";
         } else {
-            console.log("NOK");
+            console.log(response);
         }
     });
     return false;
