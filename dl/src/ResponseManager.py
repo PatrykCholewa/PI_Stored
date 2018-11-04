f42170f3ecb5d27a17e5b5f6cd1c115aba0b2b59
Patryk Cholewa <patryk.cholewa@sharpeo.com>
2018-11-04 15:16:25 +0100

    restructure

---
+++ dl/src/ResponseManager.py
@@ -0,0 +1,29 @@
+from flask import Response, abort
+
+
+def _create_response(content, content_type, status_code, status):
+    __response = Response(content)
+    __response.content_type = content_type
+    __response.status_code = status_code
+    __response.status = status
+    return __response
+
+
+def create_response_200(content, content_type):
+    return _create_response(content, content_type, 200, "200 OK")
+
+
+def create_response_401():
+    return abort(401)
+
+
+def create_response_404():
+    return abort(404)
+
+
+def create_response_403():
+    return abort(403)
+
+
+def create_response_400():
+    return abort(400)
---
+++ webapp/src/UserFileManager.py
@@ -0,0 +1,48 @@
+from os import listdir, mkdir, path
+from werkzeug.utils import secure_filename
+
+__users_dir = "db/userfiles/"
+
+
+def check_dir_exist(username):
+    dirs = listdir(__users_dir)
+    for __dir in dirs:
+        if __dir == username:
+            return
+
+    return mkdir(__users_dir + username)
+
+
+def get_user_file_names(username):
+    check_dir_exist(username)
+    files = listdir(__users_dir + username)
+    ret = '{\"files\":['
+    flag = False
+    for file in files:
+        if flag:
+            ret = ret + ","
+        else:
+            flag = True
+
+        ret = ret + '"' + file + '"'
+
+    ret = ret + "]}"
+    return ret
+
+
+def get_user_file(username, filename):
+    file = open(__users_dir + username + "/" + filename, "rb")
+    if file is None:
+        return None
+
+    return file
+
+
+def save_user_file(username, file):
+    check_dir_exist(username)
+    if len(listdir(__users_dir + username)) > 4:
+        return False
+
+    filename = secure_filename(file.filename)
+    file.save(__users_dir + username + "/" + filename)
+    return True
